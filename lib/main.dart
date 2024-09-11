238241905a30973fb2c965266048e7a7a092915d
7aneeen <haneenalaafci@gmail.com>
2024-09-11 03:38:48 +0300

    "private Api key"

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shop_app/features/splashscreen/presentation/views/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp();
   runApp(MyApp());
 }
